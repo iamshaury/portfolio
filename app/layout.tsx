a715eec97be644c8239873e03caa1cc324ba8e3a
iamshaury <officialshaury@gmail.com>
2025-10-18 17:42:38 +0530

    typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import StyledComponentsRegistry from "@/lib/registry";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Shaury's Portfoliot",
+  title: "Shaury's Portfolio",
   description: "A personal portfolio website.",
 };
 
