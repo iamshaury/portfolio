eed99b28f10b4a49815f69eeafa868520955c6e6
iamshaury <officialshaury@gmail.com>
2025-10-18 17:41:21 +0530

    changed the title

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import StyledComponentsRegistry from "@/lib/registry";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
+  title: "Shaury's Portfoliot",
   description: "A personal portfolio website.",
 };
 
